3e1850c403a45ba3a793d190886256aed7537669
Hansaka Eranda <87437151+HansakaEranda@users.noreply.github.com>
2022-05-16 18:46:31 +0530

    register student

--- controllers/studentCtrl.js
+++ controllers/studentCtrl.js
@@ -1,11 +1,61 @@
 const Students = require('../models/studentModel')
+const bycrypt = require('bcrypt') 
+const jwt = require('jsonwebtoken')
+
+const {CLIENT_URL} = process.env
 
 const studentCtrl = {
     register: async (req, res) => {
         try {
+            const {name, email, password} = req.body
+            
+            if(!name || !email || !password)
+                return res.status(400).json({msg: "Please fill all the fields."})
+
+
+            if(!validEmail(email))
+                return res.status(400).json({msg: "Invalid Email"})
+
+            const student = await Students.findOne({email})
+            if(student) return res.status(400).json({msg: "This email already exists."})
+
+            if(password.length < 6)
+                return res.status(400).json({msg: "Password must have at least 6 characters."})
+
+            const passwordHash = await bycrypt.hash(password, 12)
+            const newStudent = {
+                name, email, password: passwordHash
+            }
+            const activation_token = createActivationToken(newStudent)
+
+            const url = `${CLIENT_URL}/student/activate/${activation_token}`
+            console.log({activation_token})
+
+
             res.json({msg: "Register Test"})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     }
 }
+
+
+function validEmail(email){
+    const regex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+    return regex.test(email);
+}
+
+const createActivationToken = (payload) => {
+    return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, {expiresIn: '5m'})    
+}
+
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '5m'})    
+}
+
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '5m'})    
+}
+
+
+module.exports = studentCtrl
