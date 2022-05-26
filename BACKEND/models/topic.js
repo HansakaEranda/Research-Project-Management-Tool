394500e7c380589c42ab2415f15ea676e2fb510c
Pamudi Punsala <88242530+pamudipunsala@users.noreply.github.com>
2022-05-26 20:19:51 +0530

    backend modified

--- BACKEND/models/topic.js
+++ BACKEND/models/topic.js
@@ -1,5 +1,32 @@
-const mongoose = require('module');
+const mongoose = require('mongoose');
 
 const topicSchema = new mongoose.Schema({
 
-})
+    groupId:{
+        type:String,
+        required:true
+    },
+    groupLeader:{
+        type:String,
+        required:true
+    },
+    member1:{
+        type:String,
+        required:true
+    },
+    member2:{
+        type:String,
+        required:true
+    },
+    member3:{
+        type:String,
+        required:true
+    },
+    topic:{
+        type:String,
+        required:true
+    }
+
+});
+
+module.exports = mongoose.model('Topics', topicSchema);
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const topicRoutes = require('./routes/topic');
+
+app.use(bodyParser.json());
+
+app.use(topicRoutes);
+
 const PORT = 8000;
-const URL = "mongodb+srv://rpmt:rpmt321@cluster0.kvck9.mongodb.net/rpmt?retryWrites=true&w=majority";
+const URL = "mongodb+srv://rpmt:rpmt321@cluster0.kvck9.mongodb.net/rpmtDB?retryWrites=true&w=majority";
 
 mongoose.connect(URL)
 .then(() => {
