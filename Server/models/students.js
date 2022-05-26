d6b7cf1f08f07f64ace11b2f81881453d7cacc05
Hasini Thisari <it20225056@my.sliit.lk>
2022-05-26 12:23:56 +0530

    added student management server folder

---
+++ Server/models/students.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    stname:{
+        type:String,
+        required:true
+    },
+    regNo:{
+        type:String,
+        required:true
+    },
+    stemail:{
+        type:String,
+        required:true
+    },
+    stuserName:{
+        type:String,
+        required:true
+    },
+    stpwd:{
+        type:String,
+        required:true
+    }
+});
+
+module.exports = mongoose.model('Students',studentSchema)
