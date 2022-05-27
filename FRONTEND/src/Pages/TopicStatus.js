336851d0b9939589a077b344456bfd4c128b4fef
Pamudi Punsala <88242530+pamudipunsala@users.noreply.github.com>
2022-05-27 20:07:18 +0530

    babel/concurrently package

---
+++ FRONTEND/src/Pages/TopicStatus.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export default class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            topics:[]
+        };
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
