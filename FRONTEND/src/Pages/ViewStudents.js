180b346902e9982a3bfcc37810fa0a59919a608f
HasiThish <86554418+HasiThish@users.noreply.github.com>
2022-05-28 23:08:42 +0530

    update Pages/ ViewStudent.js

--- FRONTEND/src/Pages/ViewStudents.js
+++ FRONTEND/src/Pages/ViewStudents.js
@@ -28,17 +28,43 @@ retrieveStudents(){
 
     render(){
         return (
-            <div>
+            <div className="container">
                 <h1>Research Management Tool</h1>
-                {this.state.students.map(students => (
-                    <div>
-                        <p>{students.stname}</p>
-                        <p>{students.regNo}</p>
-                        <p>{students.stemail}</p>
-                        <p>{students.stuserName}</p>
-                        <p>{students.stpwd}</p>
-                    </div>
-                ))}
+                <p>All Students</p>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Student Name</th>
+                            <th scope="col">Register Number</th>
+                            <th scope="col">Student Email</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Password</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.students.map((students,index) => (
+                        <tr>
+                                <th scope="row">{index+1}</th>
+                                <td>{students.stname}</td>
+                                <td>{students.regNo}</td>
+                                <td>{students.stemail}</td>
+                                <td>{students.stuserName}</td>
+                                <td>{students.stpwd}</td>
+                                <td>
+                                    <a className="btn btn-warning" href="#">
+                                        <i className="fas fa-edit"></i>&nbsp;Edit
+                                    </a>
+                                    &nbsp;
+                                    <a className="btn btn-danger" href="#">
+                                        <i className="far fa-trash-alt"></i>&nbsp;Delete
+                                    </a>
+                                </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
             </div>
         )
     }
