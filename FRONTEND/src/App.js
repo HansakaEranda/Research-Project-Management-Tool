2f9f50fd91c2a358543cd8b6da402819dead9e56
Pamudi Punsala <88242530+pamudipunsala@users.noreply.github.com>
2022-05-28 19:21:26 +0530

    app.js updated

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component{
         return(
             <BrowserRouter>
                 <div>
-                    <Route path="" exact Component={}></Route>
+                    
 
                     <Route path="/addTDtl" Component={InsertTopicDtl}></Route>
                     <Route path="/addTDtl" Component={ViewStTopicDtl}></Route>
